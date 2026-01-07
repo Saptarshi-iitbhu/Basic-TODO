e0d830b0a669e16b7e72fa1ff3fa6515ee871213
Saptarshi Chakraborty <saptarshichakraborty783@gmail.com>
2026-01-07 20:27:30 +0530

    Modified index.js file and added environemnt variables

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,20 @@
-import express from "express"
-import cors from "cors"
-import bodyParser from "body-parser"
+import express from "express";
+import cors from "cors";
+import userRouter from "./routes/user.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
-const JWT_SECRET = "Saptarshi_Todo";
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
+app.use('/', userRouter);
 
-export { JWT_SECRET };
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+export default app;
+export const JWT_SECRET = process.env.JWT_SECRET;
