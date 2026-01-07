44c3730044d04c0212562c8d0667709b3e8fc50c
Saptarshi Chakraborty <saptarshichakraborty783@gmail.com>
2026-01-07 20:42:22 +0530

    Connected to the database

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import connectDB from "./db/db.js";
 import cors from "cors";
 import userRouter from "./routes/user.js";
 import dotenv from "dotenv";
@@ -12,9 +13,20 @@ app.use(cors());
 app.use(express.json());
 app.use('/', userRouter);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+const startServer = async () => {
+  try {
+    await connectDB()
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Failed to start server:", error);
+    process.exit(1);
+  }
+};
+
+startServer();
 
 export default app;
 export const JWT_SECRET = process.env.JWT_SECRET;
